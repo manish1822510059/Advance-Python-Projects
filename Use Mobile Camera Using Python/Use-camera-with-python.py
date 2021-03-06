eb085fc73c2d88a24840dfb5dc4a6fbc38a2261a
Manish kumar <manish5854758@gmail.com>
2021-03-06 22:55:51 +0530

    Add files via upload

---
+++ Use Mobile Camera Using Python/Use-camera-with-python.py
@@ -0,0 +1,12 @@
+import  cv2
+import numpy as np
+url  = "https://192.168.0.101:8080/video"
+cp = cv2.VideoCapture(url)
+while(True):
+    camera, frame = cp.read()
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+    q = cv2.waitKey(1)
+    if q==ord("q"):
+        break
+cv2.destroyAllWindows()
